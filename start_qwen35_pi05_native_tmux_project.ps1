96961b82b4c41f16c36ad13d58f59241c1d4729c
Frank Zhou <frankkzhouu@gmail.com>
2026-04-18 17:26:14 -0400

    trim repo to v3.1 architecture and train eval files

--- start_qwen35_pi05_native_tmux_project.ps1
+++
@@ -1,5 +0,0 @@
-$linuxRoot = "/home/frankkkz/qwen35_2b_vla"
-$linuxScript = "$linuxRoot/ops/tmux/launch_qwen35_pi05_native_tmux.sh"
-$argString = ($args -join " ")
-
-wsl bash -lc "chmod +x $linuxRoot/ops/tmux/*.sh && $linuxScript $argString"
